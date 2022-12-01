353da78236d5f25f0a34f793e4b0e11f28e3319a
Artsiom Fando <artsiomfando@gmail.com>
2022-12-01 17:50:20 +0100

    feat: add storybook and some stories

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import '../scss/_styles.scss';
+import 'semantic-ui-css/semantic.min.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
